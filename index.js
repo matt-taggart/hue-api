a7bc247102d9e6b13f16bd52717af189e3477d64
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-12-31 01:12:08 -0500

    Added error handling for undefined ip address.

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ const Methods = {
     const getBridgeIp = this.getBridgeIp();
 
     const parseIp = function parseIp(response) {
-      const ipAddress = JSON.parse(response)[0].internalipaddress;
+      const ipAddress = JSON.parse(response)[0].internaliaddress;
 
       return ipAddress;
     }
 
     const createUserRequest = function createUserRequest(ipAddress) {
+
+      if (!ipAddress) {
+        throw new Error('Could not retrieve ip address.');
+      }
+
       let opts = {
         method: 'POST',
         uri: `http://${ipAddress}/api`,
@@ -24,12 +29,20 @@ const Methods = {
       return request.post(opts);
     }
 
+    const parseUserId = function saveUserId(response) {
+      const usserId = JSON.parse(response)
+    }
+
     return getBridgeIp
       .then(parseIp)
       .then(createUserRequest)
+      // .then(saveUserId)
       .catch(error => {
         throw error;
       });
+  },
+  saveUser() {
+
   }
 }
 
