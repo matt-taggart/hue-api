bba57d639671a4ea2d41434fb3a0445d8d4254ec
Matthew Christopher Taggart <mtaggart89@gmail.com>
2017-01-07 01:38:35 -0500

    Refactored structure of Hue object && methods.

--- index.js
+++ index.js
@@ -1,69 +1,67 @@
 const request = require('request-promise');
+const fs = require('fs');
 
-const Methods = {
+const parseIp = function parseIp(response) {
+  const ipAddress = JSON.parse(response)[0].internalipaddress;
+
+  return ipAddress;
+}
+
+const createUserRequest = function createUserRequest(ip, app) {
+
+  if (!ipAddress) {
+    throw new Error('Failure retrieving ip address.');
+  }
+
+  let opts = {
+    method: 'POST',
+    uri: `http://${ip}/api`,
+    body: { "devicetype": `${app}` },
+    json: true
+  }
+
+  return request.post(opts);
+}
+
+const parseUserId = function parseUserId(res) {
+
+  if (res && res[0] && res[0].error) {
+    throw new Error(`Failure retrieving username: "${res[0].error}"`);
+  }
+
+  const userId = res[0].success.username;
+
+  return userId;
+}
+
+const saveUsername = function saveUsername(username) {
+
+  if (!username) {
+    throw new Error("User id is undefined.");
+  }
+
+  return { success: `user ${username} created` };
+}
+
+const Hue = {
+  init() {
+    this.ipAddress = this.getBridgeIp();
+  }
   getBridgeIp() {
     return request.get('https://www.meethue.com/api/nupnp');
   },
-  createUser() {
-    const getBridgeIp = this.getBridgeIp();
+  createUser(params) {
+    let app = params.app;
+    let ip = parseIp(this.ipAddress);
 
-    const parseIp = function parseIp(response) {
-      const ipAddress = JSON.parse(response)[0].internalipaddress;
-
-      return ipAddress;
-    }
-
-    const createUserRequest = function createUserRequest(ipAddress) {
-
-      if (!ipAddress) {
-        throw new Error('Failure retrieving ip address.');
-      }
-
-      let opts = {
-        method: 'POST',
-        uri: `http://${ipAddress}/api`,
-        body: { "devicetype": "myHueApp" },
-        json: true
-      }
-
-      return request.post(opts);
-    }
-
-    const parseUserId = function parseUserId(res) {
-
-      if (res && res[0] && res[0].error) {
-        throw new Error('Failure retrieving username.');
-      }
-
-      return userId;
-    }
-
-    const saveUsername = function saveUsername(username) {
-
-      if (!username) {
-        throw new Error("User id is undefined.");
-      }
-
-      process.env.USERNAME = username;
-
-      return { success: `user ${username} successfully created`};
-    }
-
-
-    return getBridgeIp
-      .then(parseIp)
-      .then(createUserRequest)
+    return createUserRequest(ip, app)
       .then(parseUserId)
       .then(saveUsername)
       .catch(error => {
         throw error;
       });
-  },
-  saveUser() {
 
   }
 }
 
-const Hue = Object.create(Methods);
-
-module.exports = Hue;
+module.exports = Object.create(Hue);
