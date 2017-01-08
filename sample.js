e0b2c190c19f0b9c5a9faede991cc0274fe3e37a
Matthew Christopher Taggart <mtaggart89@gmail.com>
2017-01-07 22:47:23 -0500

    Updated sample app.

--- sample.js
+++ sample.js
@@ -6,14 +6,22 @@ Hue.config({
   username: process.env.USERNAME
 });
 
-setInterval(function() {
+let count = 0;
+
+let display = setInterval(function() {
   let random = Math.floor(Math.random() * 65535);
 
-  Hue.setColor({
-    id: [1, 2, 3],
-    sat: 254,
-    bri: 254,
-    hue: random
-  });
+  if (count < 10) {
+    Hue.setColor({
+      id: [1, 2, 3],
+      sat: 254,
+      bri: 254,
+      hue: random
+    });
+  } else {
+    clearInterval(display);
+  }
+
+  count++;
 
 }, 3000);
