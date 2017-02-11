8307cf1e0429cfbbe8404e70c06c89e69661ab4b
Matthew Christopher Taggart <mtaggart89@gmail.com>
2017-02-11 16:04:19 -0500

    Refactored unit tests to use chai as promised package.

--- test/hue.js
+++ test/hue.js
@@ -1,5 +1,9 @@
+const chai = require('chai');
 const assert = require('chai').assert;
+const chaiAsPromised = require('chai-as-promised');
 const Hue = require('./../index.js');
+
+chai.use(chaiAsPromised);
 
 describe('Testing Hue API', function() {
 
@@ -16,95 +20,60 @@ describe('Testing Hue API', function() {
     });
   });
 
-  it('Should get all lights', function(done) {
-    return hue.getLights()
-      .then(results => {
-        assert.property(results, '1');
-        assert.deepProperty(results, '1.state');
-        assert.deepProperty(results, '1.state.on');
-        done();
-      });
+  it('Should get all lights', function() {
+    return assert.eventually.deepProperty(hue.getLights(), '1.state.on');
   });
 
-  it('Should get light by id', function(done) {
-    return hue.getLightById(1)
-      .then(results => {
-        assert.property(results, 'state');
-        assert.deepProperty(results, 'state.on');
-        done();
-      });
+  it('Should get light by id', function() {
+    return assert.eventually.deepProperty(hue.getLightById(1), 'state.on');
   });
 
-  it('Should turn on light with id of 1', function(done) {
-    return hue.turnOn(1)
-      .then(results => {
-        let expected = [ { success: { '/lights/1/state/on': true } } ];
-
-        assert.deepEqual(results, expected);
-        done();
-      });
+  it('Should turn off all lights', function() {
+    return assert.eventually.equal(hue.turnOffAll(), 'All lights successfully turned off.');
   });
 
-  it('Should set colors of light 1', function(done) {
-    let options = {
+  it('Should turn on light with id of 1', function() {
+    let expected = [ { success: { '/lights/1/state/on': true } } ];
+
+    return assert.eventually.deepEqual(hue.turnOn(1), expected);
+  });
+
+  it('Should set colors of light 1', function() {
+    const options = {
       id: 1,
       sat: 254,
       bri: 254,
       hue: 1000
     };
 
-    return hue.setColor(options)
-      .then(results => {
-        let expected = [
-          { success: { '/lights/1/state/hue': 1000 } },
-          { success: { '/lights/1/state/sat': 254 } },
-          { success: { '/lights/1/state/bri': 254 } }
-        ];
+    const expected = [
+      { success: { '/lights/1/state/hue': 1000 } },
+      { success: { '/lights/1/state/sat': 254 } },
+      { success: { '/lights/1/state/bri': 254 } }
+    ];
 
-        assert.deepEqual(results, expected);
-        done();
-      });
+    return assert.eventually.deepEqual(hue.setColor(options), expected);
   });
 
-  it('Should turn off light with id of 1', function(done) {
-    return hue.turnOff(1)
-      .then(results => {
-        let expected = [ { success: { '/lights/1/state/on': false } } ];
+  it('Should turn off light with id of 1', function() {
+    const expected = [ { success: { '/lights/1/state/on': false } } ];
 
-        assert.deepEqual(results, expected);
-        done();
-      });
+    return assert.eventually.deepEqual(hue.turnOff(1), expected);
   });
 
-  it('Should turn set colors of lights 1, 2, and 3', function(done) {
-    let options = {
+  it('Should turn on all lights', function() {
+    return assert.eventually.equal(hue.turnOnAll(), 'All lights successfully turned on.');
+  });
+
+  it('Should turn set colors of lights 1, 2, and 3', function() {
+    const options = {
       id: [1, 2, 3],
       sat: 254,
       bri: 254,
       hue: 50000
     };
 
-    return hue.setColor(options)
-      .then(result => {
-        assert.equal(result, 'Successfully changed lights.');
-        done();
-      });
-  });
-
-  it('Should turn off all lights', function(done) {
-    return hue.turnOffAll()
-      .then(result => {
-        assert.equal(result, 'All lights successfully turned off.');
-        done();
-      });
-  });
-
-  it('Should turn on all lights', function(done) {
-    return hue.turnOnAll()
-      .then(result => {
-        assert.equal(result, 'All lights successfully turned on.');
-        done();
-      });
+    return assert.eventually.deepEqual(hue.setColor(options), 'Successfully changed lights.');
   });
 
 });
