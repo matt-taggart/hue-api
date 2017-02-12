e255a0f41a2c7c39cd3696b56d818524432fc0de
Matthew Christopher Taggart <mtaggart89@gmail.com>
2017-02-11 20:35:04 -0500

    Wrote check for id in turn on and turn off functions.

--- methods/turnOff.js
+++ methods/turnOff.js
@@ -1,6 +1,10 @@
 const request = require('request-promise');
 
 const turnOff = function turnOff(id) {
+  if (!id) {
+    throw new Error("Must provide light id(s) as an argument.");
+  }
+  
   let params = {
     method: 'PUT',
     uri: `http://${this.ip}/api/${this.username}/lights/${id}/state`,
